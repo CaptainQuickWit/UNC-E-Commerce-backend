cc71b1bc71515e9667f79ebafea0eaff9392620c
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-19 03:05:08 -0400

    added a model reference to product in index models folder

--- models/index.js
+++ models/index.js
@@ -14,7 +14,10 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-
+  through: {
+    model: ProductTag,
+      unique: false
+    },
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
