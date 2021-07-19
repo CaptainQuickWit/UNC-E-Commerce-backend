30fbf9d5e46cbe2064a40ed1c97075bac9b27ccc
Chetan Patel <Chetan@Chetans-MacBook-Air.local>
2021-07-19 03:11:38 -0400

    added some routes these rouets are working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,24 +6,81 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll(
-    {
-      include: {
-        model: Product,
-        attributes: ['product_name']
-      }
+  Category.findAll({
+
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+
     }
-  )
+
+  }).then(data => {
+
+    if(!data) {
+      res.status(400).json({message: 'The categories were not found please try again'});
+      return;
+    }
+
+    res.status(200).json(data);
+
+  }).catch(err => {
+
+    console.log(err);
+    res.status(400).json(err);
+
+  });
 
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  Category.findOne({
+
+    where: {
+      id: req.params.id
+    },
+
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name']
+    }
+
+
+  }).then(data => {
+
+    if(!data) {
+
+      res.status(400).json({message: 'search results: none'})
+      return;
+    }
+    res.status(200).json(data);
+
+  }).catch(err => {
+
+    console.log(err);
+    res.status(400).json(err);
+
+  });
+
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.update({
+  category_name: req.body.category_name
+  }).then(data => res.status(200).json(data))
+
+  .catch(err => {
+
+    console.log(err);
+
+    res.status(400).json(err);
+
+  });
+
 });
 
 router.put('/:id', (req, res) => {
